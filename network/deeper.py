4035057150f7f66adf4a93080c2c8a4dcfabf381
huangfeifei <huangfeifei@aidigger.com>
2020-10-28 11:19:25 +0800

    Added the dpc in deeperx71

--- network/deeper.py
+++ network/deeper.py
@@ -11,13 +11,13 @@ class DeeperS8(nn.Module):
     Panoptic DeepLab-style semantic segmentation network
     stride8 only
     """
-    def __init__(self, num_classes, trunk="xception71", criterion=None):
+    def __init__(self, num_classes, trunk="xception71", criterion=None, dpc=False):
         super(DeeperS8, self).__init__()
 
         self.criterion = criterion
         self.backbone, s2_ch, s4_ch, high_level_ch = get_trunk(trunk_name=trunk, output_stride=8)
         # TODO: DPC modules
-        self.aspp, aspp_out_ch = get_aspp(high_level_ch, bottleneck_ch=256, output_stride=8)
+        self.aspp, aspp_out_ch = get_aspp(high_level_ch, bottleneck_ch=256, output_stride=8, dpc=dpc)
 
         self.convs2 = nn.Conv2d(s2_ch, 32, kernel_size=1, bias=False)
         self.convs4 = nn.Conv2d(s4_ch, 64, kernel_size=1, bias=False)
@@ -195,6 +195,11 @@ class MscaleDeeperS8(nn.Module):
 def DeeperX71(num_classes, criterion=None):
 
     return DeeperS8(num_classes, criterion=criterion, trunk="xception71")
+
+
+def DeeperX71_DPC(num_classes, criterion=None):
+
+    return DeeperS8(num_classes, criterion=criterion, trunk="xception71", dpc=True)
 
 
 def DeeperRX101(num_classes, criterion=None):
